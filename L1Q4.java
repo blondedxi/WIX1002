e193e96022d5c6f77a5d141257fa57e64b964abf
NUR FATNIN JAZMINA BINTI JIMMY <fanaz6459@gmail.com>
2024-10-24 11:20:18 +0800

    Add files via upload

---
+++ L1Q4.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author miema
+ */
+public class L1Q4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("                   Total Sales of Product A in 2016");
+        System.out.println("                                                   ");
+        System.out.println("3200 |                                                  *******");
+        System.out.println("     |                                                  *******");
+        System.out.println("     |                                                  *******");       
+        System.out.println("     |                                                  *******");  
+        System.out.println(" 2800|                                                  *******"); 
+        System.out.println("     |                                       *******    *******    ");   
+        System.out.println("     |                                       *******    *******    ");
+        System.out.println("     |      *******                          *******    *******    ");
+        System.out.println(" 2400|      *******                          *******    *******    ");       
+        System.out.println("     |      *******                          *******    *******    "); 
+        System.out.println("     |      *******                          *******    *******    ");        
+        System.out.println("     |      *******                          *******    *******    ");           
+        System.out.println(" 2000|      *******               *******    *******    *******    ");
+        System.out.println("     |      *******               *******    *******    *******    ");
+        System.out.println("     |      *******               *******    *******    *******    ");        
+        System.out.println("     |      *******               *******    *******    *******    ");        
+        System.out.println(" 1600|      *******    *******    *******    *******    *******    ");        
+        System.out.println("     |      *******    *******    *******    *******    *******   ");         
+        System.out.println("     |      *******    *******    *******    *******    *******    ");         
+        System.out.println("     |      *******    *******    *******    *******    *******    ");         
+        System.out.println(" 1200|      *******    *******    *******    *******    *******    ");         
+        System.out.println("     |      *******    *******    *******    *******    *******    ");         
+        System.out.println("     |      *******    *******    *******    *******    *******    ");         
+        System.out.println("     |      *******    *******    *******    *******    *******    ");         
+        System.out.println(" 800 |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println(" 400 |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("     |      *******    *******    *******    *******    *******    *******");         
+        System.out.println("   0 |______*******____*******____*******____*******____*******____*******__");
+        System.out.println("            January    February    March      April       May       June");
+      
+                
+                
+                
+                
+                
+                
+                
+                
+                
+    }
+    
+    
+    
+}
