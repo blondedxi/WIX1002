3023eb0818807f539d9c9fb6ee3fb7b82904d56e
NUR FATNIN JAZMINA BINTI JIMMY <fanaz6459@gmail.com>
2024-10-24 11:17:48 +0800

    Add files via upload

---
+++ MyProfile.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author miema
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name: Nur Fatnin Jazmina");
+        System.out.println("Matric num: 24004490");
+        System.out.println("Address: No.9,Jalan 7/1d5,Seksyen 7, 43650 Bandar Baru Bangi");
+        System.out.println("Email:fanaz6459@gmail.com");
+        System.out.println("Contact: 0133501159");
+        System.out.println("Hobby: Playing basketball & Listening to music");
+        
+        
+                
+                
+
+               
+    }
+    
+}
